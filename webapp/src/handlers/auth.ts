a6dddc0d0d65b88257070ba4e517ef01d0e9d06a
Jacute <Eri228@yandex.ru>
2024-07-14 13:00:44 +0700

    change import utils

--- webapp/src/handlers/auth.ts
+++ webapp/src/handlers/auth.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
-import { comparePassword, hashPassword } from '../utils';
+import { comparePassword, hashPassword } from '../utils/utils';
 import * as db from '../database';
-import { encodeToken, verifyToken } from '../utils/jwt';
-import * as config from '../config';
 
 
 async function register(req: Request, res: Response) {
@@ -33,8 +31,8 @@ async function register(req: Request, res: Response) {
         res.status(500).json({message: "Internal Server Error."});
         return;
     }
-    const token = encodeToken(username);
-    res.status(201).cookie("auth-token", token, {maxAge: config.COOKIE_LIFETIME}).json({message: "User registered successfully."});
+    req.session.username = username;
+    res.status(201).json({message: "User registered successfully."});;
 }
 
 async function login(req: Request, res: Response) {
@@ -51,17 +49,17 @@ async function login(req: Request, res: Response) {
         res.status(401).json({message: "Invalid username or password."});
         return;
     }
-    const token = encodeToken(username);
-    res.status(200).cookie("auth-token", token).json({message: "User logged in successfully."});;
+    req.session.username = username;
+    res.status(200).json({message: "User logged in successfully."});;
 }
 
 async function logout(req: Request, res: Response) {
     req.session.destroy(err => {
         if (err) {
-            return res.status(500).json({ message: "Internal Server Error." });
+            return res.status(500).json({message: "Internal Server Error."});
         }
-        res.cookie("auth-token", "").redirect(req.headers.referer ? req.headers.referer : "/");
     });
+    res.redirect(req.headers.referer ? req.headers.referer : "/");
 }
 
 export { register, login, logout };
