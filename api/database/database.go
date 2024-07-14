b8fd9f48a5e8566b26606a0cbe6049d8aae38a06
Jacute <Eri228@yandex.ru>
2024-07-14 12:52:45 +0700

    transferring test recipes to init.sql, changes in docker

---
+++ api/database/database.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"RecipeBookApi/config"
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	DB *sql.DB
+)
+
+func СonnectMySQL() {
+	var err error
+
+	connectionString := config.GetMySQL()
+
+	DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println("Successfully connected to MySQL")
+}
